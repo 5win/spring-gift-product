b1f1cc22be3fe5edb9b16cfa5c248ce77148931e
5win <woozzoo7@gmail.com>
2024-07-01 00:11:34 +0900

    style: Changed to fit the google coding convention

--- src/test/java/gift/ProductDaoTest.java
+++ src/test/java/gift/ProductDaoTest.java
@@ -1,15 +1,14 @@
 package gift;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 class ProductDaoTest {
